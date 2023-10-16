9d7ab97194deca6ab5f028242cbaf4b88df8e902
StfnsFerry <stefanusferry.05@gmail.com>
2023-10-16 11:25:58 +0700

    helper

---
+++ lib/helper.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+// late String? nama;
+void main() {
+  // String? nama;
+
+  // int umur = 20;
+  // final String? nama = "Saya";
+  // print(nama);
+
+  // stdout.write("Masukkan nama Anda: ");
+
+  // String? message;
+  // String text;
+
+  // if (message == null) {
+  //   text = "Empty";
+  //   print(text);
+  // } else {
+  //   text = message;
+  // }
+
+  // String text = message ?? "Empty";
+
+  List angka = [1, 2, 3, 4, 5];
+  printList(angka);
+}
+
+void printList(angka) {
+  for (int i = 0; i < angka.length; i++) {
+    print(angka[i]);
+  }
+}
